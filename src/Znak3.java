9e8f7d2a3fcdd83bdb9724e153b880d64ae7efff
Stefan Vladisavljevic <stefanv1710@gmail.com>
2018-02-06 21:18:20 +0100

    Commit d1

--- src/Znak3.java
+++ src/Znak3.java
@@ -7,7 +7,7 @@ public class Znak3 extends JPanel {
 		
 		super.paintComponent(g);
 		
-		
+		//Neki komentar
 		int[] xs2={118,320,522,320};
 		int[] ys2={240,38,240,442};
 		g.setColor(Color.BLACK);
